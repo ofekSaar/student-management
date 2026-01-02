de5b7db620a6c89d6e5c4869d8b4f4e3cbfe0117
nivshapira211@gmail.com <niv@Nivs-MacBook-Air.local>
2026-01-02 17:15:31 +0200

    added divider between studetnts

--- app/src/main/java/com/example/student_management/StudentListActivity.kt
+++ app/src/main/java/com/example/student_management/StudentListActivity.kt
@@ -3,6 +3,7 @@ package com.example.student_management
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.student_management.databinding.ActivityStudentListBinding
 import com.example.student_management.model.Model
@@ -16,7 +17,16 @@ class StudentListActivity : AppCompatActivity() {
         binding = ActivityStudentListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.studentsRecyclerView.layoutManager = LinearLayoutManager(this)
+        val layoutManager = LinearLayoutManager(this)
+        binding.studentsRecyclerView.layoutManager = layoutManager
+        
+        // Add divider
+        val dividerItemDecoration = DividerItemDecoration(
+            binding.studentsRecyclerView.context,
+            layoutManager.orientation
+        )
+        binding.studentsRecyclerView.addItemDecoration(dividerItemDecoration)
+
         adapter = StudentAdapter(Model.students) { position ->
             val intent = Intent(this, StudentDetailsActivity::class.java)
             intent.putExtra("student_position", position)
